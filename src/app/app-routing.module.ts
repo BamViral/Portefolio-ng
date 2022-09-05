27a4028aa9eedde123d0bac8ef81d4fe12d5cf11
BamViral <bam453@msn.com>
2022-09-05 20:00:59 +0200

    mise en place du routing, mise en place d'un carousel, création des composant de base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
+import { ErrorComponent } from './error/error.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { SoundComponent } from './sound/sound.component';
+import { StreamComponent } from './stream/stream.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:CarouselComponent},
+  {path:'about',component:AboutComponent},
+  {path:'stream',component:StreamComponent},
+  {path:'sound',component:SoundComponent},
+  {path:'**',component:ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CarouselComponent } from './carousel/carousel.component';
+import { AboutComponent } from './about/about.component';
+import { ErrorComponent } from './error/error.component';
+import { SoundComponent } from './sound/sound.component';
+import { StreamComponent } from './stream/stream.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    CarouselComponent,
+    AboutComponent,
+    ErrorComponent,
+    SoundComponent,
+    StreamComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
